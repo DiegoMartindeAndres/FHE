2547c2dcf9769ffccedd1013db00ac38b4072b87
Patricia Ortuño <patricia.ortuno28@gmail.com>
2022-03-27 21:52:09 +0200

    Trabajando en client/server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 // Settings
 app.set('PORT', process.env.PORT || 3000); // The cloud's defined port || predefined port 3000
@@ -10,12 +11,16 @@ app.set('json spaces', 2); // JSON format
 app.use(morgan('dev')); // Obtain HTTP requests sent to the server
 app.use(express.urlencoded({extended: false}));
 app.use(express.json()); // Server understands JSON format
+app.use(bodyParser.json({limit: "10000kb"}));
+app.use(bodyParser.urlencoded({limit: "10000kb", extended: true}));
 
 // Routes
 app.use('/api/sumbfv', require('./routes/sumbfv.js'));
 app.use('/api/sumckks', require('./routes/sumckks.js'));
 app.use('/api/parms-linear', require('./routes/parmsLinear.js'));
 app.use('/api/compute-linear', require('./routes/computeLinear.js'));
+app.use('/api/encode', require('./routes/encode.js'));
+app.use('/api/operate', require('./routes/operate.js'));
 
 // Starting server -> npm run dev
 app.listen(app.get('PORT'), () => {console.log(`Server on port ${app.get('PORT')}`)});
---
+++ src/routes/operate.js
@@ -0,0 +1,76 @@
+const {Router} = require('express');
+const router = Router();
+const _ = require('underscore');
+
+const axios = require('axios');
+
+router.get('/', async (req, res) => {
+
+    let val1 = JSON.parse(req.body.val1);
+
+    // Require SEAL
+    const SEAL = require('node-seal');
+    const seal = await SEAL();
+
+    // Scheme parameters
+    const schemeType = seal.SchemeType.bfv;
+    const securityLevel = seal.SecurityLevel.tc128; //TODO: averiguar niveles
+    const polyModulusDegree = 4096;
+    const bitSizes = [36, 36, 37];
+    const bitSize = 20;
+    
+    const parms = seal.EncryptionParameters(schemeType);
+    
+    // Set the PolyModulusDegree
+    parms.setPolyModulusDegree(polyModulusDegree);
+    
+    // Create a suitable set of CoeffModulus primes
+    parms.setCoeffModulus(
+        seal.CoeffModulus.Create(polyModulusDegree, Int32Array.from(bitSizes))
+    );
+    
+    // Set the PlainModulus to a prime of bitSize 20
+    parms.setPlainModulus(
+        seal.PlainModulus.Batching(polyModulusDegree, bitSize)
+    );
+    
+    const context = seal.Context(
+        parms, // Encryption Parameters
+        true, // ExpandModChain
+        securityLevel // Enforce a security level
+    );
+    
+    if (!context.parametersSet()) {
+        throw new Error(
+        'Could not set the parameters in the given context. Please try different encryption parameters.'
+        )
+    };
+    
+    // Homomorphic objects for the computations
+    const encoder = seal.BatchEncoder(context);
+    const keyGenerator = seal.KeyGenerator(context);
+    const publicKey = keyGenerator.createPublicKey();
+    const secretKey = keyGenerator.secretKey();
+    const relinKey = keyGenerator.createRelinKeys();
+    const encryptor = seal.Encryptor(context, publicKey);
+    const decryptor = seal.Decryptor(context, secretKey);
+    const evaluator = seal.Evaluator(context);
+
+    // Ciphertexts
+    const uploadedCipherText = seal.CipherText();
+    uploadedCipherText.load(context, val1);
+
+    const arrayB = Int32Array.from([2]);
+    const plainTextB = encoder.encode(arrayB);
+    const cipherTextB = encryptor.encrypt(plainTextB);
+    
+    // Add the CipherText
+    const cipherTextD = seal.CipherText();
+    evaluator.add(uploadedCipherText, cipherTextB, cipherTextD);
+    
+    let cipherTextDBase64 = cipherTextD.save();
+
+    res.send(cipherTextDBase64);
+})
+
+module.exports = router;
