b9d6ee56227ead90513e892b3ed5bfef1e533a7b
Patricia Ortuño <patricia.ortuno28@gmail.com>
2022-04-04 12:17:15 +0200

    Last touch up for first version

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Change Log
+
+Todos los cambios relevantes de este proyecto serán reflejados en este documento.
+
+## v1.0
+### 04/04/2022
+
+[+] README.md.
+[+] UI para que el usuario interaccione con el servicio.
+[+] Botón de inicialización (ON/OFF) de los parámetros de Microsoft SEAL.
+[+] Subservicio de cálculo de los parámetros de la regresión lineal.
+[+] Subservicio de cálculo de la predicción conocida la ecuación de regresión lineal.
+[+] Línea temporal para la comunicación cliente-servidor del primer subservicio.
+[+] Línea temporal para la comunicación cliente-servidor del segundo subservicio.
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import React from "react";
 import LoadingSpinner from './LoadingSpinner';
 import {ReactComponent as PowerOnOff} from './res/powerOnOff.svg';
+import { ReactComponent as Circle } from './res/circle.svg';
 
 const SEAL = require('node-seal');
 
@@ -37,6 +38,13 @@ function App() {
   const [divDisabled, setDivDisabled] = React.useState(true);
   const [textColorDisabledParms, setTextColorDisabledParms] = React.useState('rgb(150, 150, 150)');
   const [textColorDisabledPredict, setTextColorDisabledPredict] = React.useState('rgb(150, 150, 150)');
+
+  const [fillCircleOne, setFillCircleOne] = React.useState('white');
+  const [fillCircleTwo, setFillCircleTwo] = React.useState('white');
+  const [fillCircleThree, setFillCircleThree] = React.useState('white');
+  const [fillCircleOnePredict, setFillCircleOnePredict] = React.useState('white');
+  const [fillCircleTwoPredict, setFillCircleTwoPredict] = React.useState('white');
+  const [fillCircleThreePredict, setFillCircleThreePredict] = React.useState('white');
 
   /**************************************************
    * INITIALIZE SEAL VARIABLES
@@ -142,6 +150,7 @@ function App() {
     // Inform client that encryption has begun
     setComputingParms(true);
     setServerAnswer(`Encriptando...`);
+    setFillCircleOne('rgba(0, 255, 0, 0.9)');
 
     /**************************************************
      * OBTAIN AXIS ARRAYS
@@ -220,11 +229,13 @@ function App() {
 
     // Inform client that the server is computing the data
     setServerAnswer(`Esperando respuesta del servidor...`);
+    setFillCircleTwo('rgba(0, 255, 0, 0.9)');
 
     await axios.post('http://localhost:3000/api/parms-linear-reg', postData, axiosConfig)
     .then(res => {
       // Inform client that the decryption has begun
       setServerAnswer(`Desencriptando...`);
+      setFillCircleThree('rgba(0, 255, 0, 0.9)');
 
       /**
        * The format of the JSON to be received is the following:
@@ -283,6 +294,7 @@ function App() {
     // Inform client that encryption has begun
     setComputingPredict(true);
     setServerAnswerTwo(`Encriptando...`);
+    setFillCircleOnePredict('rgba(0, 255, 0, 0.9)');
 
     /**************************************************
      * OBTAIN X VALUE FOR PREDICTION
@@ -331,11 +343,13 @@ function App() {
 
     // Inform client that the server is computing the data
     setServerAnswerTwo(`Esperando respuesta del servidor...`);
+    setFillCircleTwoPredict('rgba(0, 255, 0, 0.9)');
 
     await axios.post('http://localhost:3000/api/predict-linear-reg', postData, axiosConfig)
     .then(res => {
       // Inform client that the decryption has begun
       setServerAnswerTwo(`Desencriptando...`);
+      setFillCircleThreePredict('rgba(0, 255, 0, 0.9)');
 
       /**
        * The format of the JSON to be received is the following:
@@ -370,35 +384,132 @@ function App() {
   }
 
   return (
-    <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <div style={{display: 'flex', flexDirection: 'row'}}>
       <button onClick={initSEAL} className="btnInitSEAL">
         {creatingParms ? <LoadingSpinner/> : 
         (powerOff ? <PowerOnOff className='icon__off' fill='rgba(255, 0, 0, 0.9)'/> : 
         <PowerOnOff className='icon__on' fill='rgba(0, 255, 0, 0.9)'/>)}
       </button>
-      <div className="firstForm">
-        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%'}}>
-          <h4 style={{color: textColorDisabledParms}}>Calcular parámetros para la regresión lineal,</h4>
-          <input type="text" className="inputFirstForm" name="X axis" id="valuesXId" 
-          placeholder='Insertar valores en X' disabled={divDisabledParms}/>
-          <input type="text" className="inputFirstForm" name="Y axis" id="valuesYId" 
-          placeholder='Insertar valores en Y' disabled={divDisabledParms}/>
-        </form>
-        <button onClick={callServer} className="btnFirstForm" disabled={divDisabledParms}>
-            {computingParms ? <LoadingSpinner/> : 'Enviar'}
-        </button>
-        <p>{serverAnswer}</p>
-      </div>
-      <div className="firstForm" style={{marginTop: '3%'}}>
-        <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%'}}>
-          <h4 style={{color: textColorDisabledPredict}}>Calcular predicción,</h4>
-          <input type="text" className="inputFirstForm" name="predict" id="valuePredict" 
-            placeholder='Insertar valor en X' disabled={divDisabled}/>
-        </form>
-        <button onClick={callServerPredict} className="btnFirstForm" disabled={divDisabled}>
-          {computingPredict ? <LoadingSpinner/> : 'Enviar'}
-        </button>
-        <p>{serverAnswerTwo}</p>
+      <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <div className="firstForm">
+            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%'}}>
+              <h4 style={{color: textColorDisabledParms}}>Calcular parámetros para la regresión lineal: pendiente (m) y punto de corte con el eje Y (b)</h4>
+              <input type="text" className="inputFirstForm" name="X axis" id="valuesXId" 
+              placeholder='Insertar valores en X' disabled={divDisabledParms}/>
+              <input type="text" className="inputFirstForm" name="Y axis" id="valuesYId" 
+              placeholder='Insertar valores en Y' disabled={divDisabledParms}/>
+            </form>
+            <button onClick={callServer} className="btnFirstForm" disabled={divDisabledParms}>
+                {computingParms ? <LoadingSpinner/> : 'Enviar'}
+            </button>
+            <p>{serverAnswer}</p>
+          </div>
+          <div className='show__timeline'>
+            <ul class="timeline">
+              <li>
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleOne} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Cliente</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Encripta los ejes X e Y y los envía al servidor.</p>
+                      </div>
+                  </div>
+              </li>
+              <li class="timeline-inverted">
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleTwo} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Servidor</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Calcula la pendiente y el punto de corte en el eje Y con los datos encriptados recibidos.
+                            Los reenvía al cliente aun encriptados. No conoce la clave privada.
+                          </p>
+                      </div>
+                  </div>
+              </li>
+              <li>
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleThree} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Cliente</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Desencripta los parámetros recibidos con la clave privada y muestra la ecuación lineal.</p>
+                      </div>
+                  </div>
+              </li>
+              <li class="clearfix no-float"></li>
+            </ul>
+          </div>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <div className="firstForm" style={{marginTop: '3%'}}>
+            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '2%'}}>
+              <h4 style={{color: textColorDisabledPredict}}>Calcular predicción en el eje Y conocido el valor en el eje X</h4>
+              <input type="text" className="inputFirstForm" name="predict" id="valuePredict" 
+                placeholder='Insertar valor en X' disabled={divDisabled}/>
+            </form>
+            <button onClick={callServerPredict} className="btnFirstForm" disabled={divDisabled}>
+              {computingPredict ? <LoadingSpinner/> : 'Enviar'}
+            </button>
+            <p>{serverAnswerTwo}</p>
+          </div>
+          <div className='show__timeline'>
+            <ul class="timeline">
+              <li>
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleOnePredict} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Cliente</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Encripta la pendiente y el punto de corte obtenidos anteriormente junto con el valor en X para la predicción y los envía al servidor.</p>
+                      </div>
+                  </div>
+              </li>
+              <li class="timeline-inverted">
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleTwoPredict} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Servidor</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Calcula la predicción con la ecuación 'y = mx + b' en el eje Y y la reenvía encriptada al cliente. No conoce la clave privada.
+                          </p>
+                      </div>
+                  </div>
+              </li>
+              <li>
+                  <div class="timeline-badge">
+                    <Circle fill={fillCircleThreePredict} className='icon__circle'/>
+                  </div>
+                  <div class="timeline-panel">
+                      <div class="timeline-heading">
+                          <h4>Cliente</h4>
+                      </div>
+                      <div class="timeline-body">
+                          <p>Desencripta la predicción obtenida y la muestra por pantalla.</p>
+                      </div>
+                  </div>
+              </li>
+              <li class="clearfix no-float"></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
